4fe2e578fb8869547cbcbf866c8a0115969e4429
yo16 <xxyo1xx@gmail.com>
2024-02-04 00:57:53 +0900

    add mahjong font

--- fu_try/src/App.js
+++ fu_try/src/App.js
@@ -1,11 +1,30 @@
 import { useState } from "react";
 
+import "./App.css";
+
 const App = () => {
-    const [isMenzen, setIsMenzen]
+    const [isMenzen, setIsMenzen] = useState(true);
+    const [isRon, setIsRon] = useState(true);
+
+    function getRandomInt(max){
+        return Math.floor(Math.random() * max);
+    }
+    function getRandomBool(){
+        return getRandomInt(2)===0?false: true;
+    }
+
+    const handleClickMakingQ = () => {
+        setIsMenzen(getRandomBool());
+        setIsRon(getRandomBool());
+    }
 
     return (
         <>
-
+            <button onClick={handleClickMakingQ}>問題作成</button>
+            <hr />
+            <div>{isMenzen?"メンゼン":"フーロ"}</div>
+            <div>{isRon?"ロン":"ツモ"}</div>
+            <div className="font-mahjong">qqyyasdm,. 9119 y</div>
         </>
     );
 };
